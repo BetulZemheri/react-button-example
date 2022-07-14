83507f19d9f4506454b8166b9acdd511a05f4fbb
Betul Zemheri <a.betul2200@gmail.com>
2022-07-14 12:15:12 +0300

    button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [deger, setDeger] = useState(0);
+
+  const Azalt = () => {
+    setDeger(deger - 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => {setDeger(deger + 1)}}>INCREASE</button>
+      <p> {deger} </p>
+      <button onClick={Azalt}>DECREASE</button>
     </div>
   );
 }
